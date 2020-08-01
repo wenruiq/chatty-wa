084612be2a6fa9dcaab88703d6273e3303db4574
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-08-01 13:52:36 +0800

    first commit

---
+++ client/src/js/firebase/firebase.utils.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAOHpgA68_RocikhcCulH_tLuFZMbydUM8",
+  authDomain: "chatty-wa.firebaseapp.com",
+  databaseURL: "https://chatty-wa.firebaseio.com",
+  projectId: "chatty-wa",
+  storageBucket: "chatty-wa.appspot.com",
+  messagingSenderId: "693027905253",
+  appId: "1:693027905253:web:55fb22096e2a00bf817e0c"
+};
