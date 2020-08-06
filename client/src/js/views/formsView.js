3ce851ac7127886a27015c57da3d11b2c57bc09e
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-08-06 19:46:57 +0800

    improved css for login page

--- client/src/js/views/formsView.js
+++ client/src/js/views/formsView.js
@@ -2,7 +2,11 @@ export const shrinkLabels = () => {
   const inputArr = Array.from(document.querySelectorAll('.form-input'));
   inputArr.forEach(el => {
     el.addEventListener('change', () => {
-      el.nextElementSibling.classList.add('shrink');
+      if (el.value.length > 0) {
+        el.nextElementSibling.classList.add('shrink');
+      } else {
+        el.nextElementSibling.classList.remove('shrink');
+      }
     });
   });
 };
